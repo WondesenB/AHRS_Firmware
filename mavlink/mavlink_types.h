c6b37f51510784755e77d02da4fb4c9827a8bff4
wondesenb <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>
2021-07-10 18:45:01 +0900

    errors related to ti_compiler resolved

--- mavlink/mavlink_types.h
+++ mavlink/mavlink_types.h
@@ -7,14 +7,15 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include "minimal/stdint_.h"
 
 #ifdef MAVLINK_USE_CXX_NAMESPACE
 namespace mavlink {
 #endif
 
 // Macro to define packed structures
-#ifdef __GNUC__
-  #define MAVPACKED( __Declaration__ ) __Declaration__ __attribute__((packed))
+#ifdef __TI_COMPILER_VERSION__  //__GNUC__
+  #define MAVPACKED( __Declaration__ ) __Declaration__  //__attribute__((packed))
 #else
   #define MAVPACKED( __Declaration__ ) __pragma( pack(push, 1) ) __Declaration__ __pragma( pack(pop) )
 #endif
